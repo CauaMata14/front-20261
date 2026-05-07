e53f8f7718bbf602eacea5e66bfaa2b7cd5857ab
Cauã <cauammta141003@gmail.com>
2026-05-07 20:42:08 -0300

    fix: corrigindo conflito de merge

--- praticas/aluno-online-react/src/App.jsx
+++ praticas/aluno-online-react/src/App.jsx
@@ -5,18 +5,12 @@ import Faltas from './pages/Faltas'
 import Notas from './pages/Notas'
 import Boletos from './pages/Boletos'
 import Requerimentos from './pages/Requerimentos'
-import Login from "./pages/Login";
-import Dashboard from "./pages/Dashboard";
-import Notas from "./pages/Notas";
-import Faltas from "./pages/Faltas";
-import Boletos from "./pages/Boletos";
-import Requerimentos from "./pages/Requerimentos";
-
+import Login from "./pages/Login"
 function App() {
-  const pagina = 0;
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
+      
         <Route index element={<Dashboard />} />
         <Route path="faltas" element={<Faltas />} />
         <Route path="notas" element={<Notas />} />
@@ -24,15 +18,6 @@ function App() {
         <Route path="requerimentos" element={<Requerimentos />} />
       </Route>
     </Routes>
-    <>
-      {pagina == 0 && <Login />}
-      {pagina == 1 && <Dashboard />}
-      {pagina == 2 && <Notas />}
-      {pagina == 3 && <Faltas />}
-      {pagina == 4 && <Boletos />}
-      {pagina == 5 && <Requerimentos />}
-    </>
   )
 }
-
 export default App
